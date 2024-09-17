3b043bd1ddad574a1f5df6b0fae5b05b6a6b2244
shodan <mega@ioexception.at>
2024-09-17 23:07:15 +0200

    lint

--- main.gd
+++ main.gd
@@ -5,7 +5,7 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	pass 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -27,7 +27,7 @@ func new_game() -> void:
 	$Music.play()
 	$HUD.show_message("Get Ready")
 
-func _on_score_timer_timeout() -> void:
+func _on_score_timer_timeout() -> void: 
 	score += 1
 	$HUD.update_score(score)
 
