17058976c01427e704b2a4453ec2aa8980d2d681
shodan <mega@ioexception.at>
2024-10-07 00:55:21 +0200

    rope

--- agents/rope/rope.gd
+++ agents/rope/rope.gd
@@ -1,11 +1,65 @@
+@tool
 extends Node2D
 
+@export_range(2, 100) var size: int:
+	set(value):
+		size = value
+		_init_segments()
+@export var segment_start: Texture2D:
+	set(value):
+		segment_start = value
+		_init_segments()
+@export var segment_middle: Texture2D:
+	set(value):
+		segment_middle = value
+		_init_segments()
+@export var segment_end: Texture2D:
+	set(value):
+		segment_end = value
+		_init_segments()
+@export var anchor: Node2D:
+	set(value):
+		anchor = value
+		_init_segments()
+@export var collision: bool = true:
+	set(value):
+		collision = value
+		_init_segments()
 
-# Called when the node enters the scene tree for the first time.
+@onready var _last_segment: Node2D
+
 func _ready() -> void:
-	pass # Replace with function body.
+	_init_segments()
+	
+func _init_segments() -> void:
+	_clear_segments()
+	_build_segments()
+	
+func _clear_segments() -> void:
+	for child in get_children():
+		if child is RopeSegment:
+			child.queue_free()
+	
+func _build_segments() -> void:
+	if segment_start == null:
+		return
+	if segment_middle == null and size > 2:
+		return
+	if segment_end == null:
+		return
+	_last_segment = _append_segment(segment_start, anchor)
+	for i in (size - 2):
+		_last_segment = _append_segment(segment_middle, _last_segment)
+	_last_segment = _append_segment(segment_end, _last_segment)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _append_segment(texture: Texture2D, parent: Node2D = null) -> Node2D:
+	var new_segment = preload("res://agents/rope/rope_segment.tscn").instantiate()
+	new_segment.texture = texture
+	if parent != null:
+		new_segment.anchor = parent.get_path()
+	new_segment.collision = collision
+	if parent is RopeSegment:
+		new_segment.position.x = parent.position.x
+		new_segment.position.x += parent.texture.get_width()
+	add_child(new_segment)
+	return new_segment
---
+++ agents/rope/rope_segment.gd
@@ -0,0 +1,53 @@
+@tool
+class_name RopeSegment extends Node2D
+
+@export var collision: bool:
+	get(): return _collision
+	set(value):
+		_collision = value
+		if $CollisionShape2D != null:
+			$CollisionShape2D.disabled = not value
+
+@export var texture: Texture2D:
+	get(): return _texture
+	set(value):
+		_texture = value
+		if $Sprite2D != null:
+			$Sprite2D.texture = value
+			_update_shape()
+
+@export var anchor: NodePath:
+	get(): return _anchor
+	set(value):
+		_anchor = value
+		if $PinJoint2D != null:
+			$PinJoint2D.node_a = value
+
+# Private backing fields to store values until nodes are ready
+var _collision: bool = false
+var _texture: Texture2D
+var _anchor: NodePath
+
+func _ready() -> void:
+	collision = _collision
+	texture = _texture
+	anchor = _anchor
+	_update_shape()
+
+func _update_shape() -> void:
+	var collision_shape = $CollisionShape2D.shape as CapsuleShape2D
+	var width = texture.get_width()
+	var height = texture.get_height()
+	
+	if height > width:
+		$CollisionShape2D.rotation_degrees = 0
+		$CollisionShape2D.position.x = height / 2.0
+		$Sprite2D.position.x = height / 2.0
+		collision_shape.radius = width / 2.0
+		collision_shape.height = height
+	else:
+		$CollisionShape2D.rotation_degrees = 90
+		$CollisionShape2D.position.x = width / 2.0
+		$Sprite2D.position.x = width / 2.0
+		collision_shape.radius = height / 2.0
+		collision_shape.height = width
