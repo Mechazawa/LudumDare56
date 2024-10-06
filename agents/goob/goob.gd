254f459f28cd9bfca826507de0874e38706c4e61
shodan <mega@ioexception.at>
2024-10-06 19:27:17 +0200

    goob ai, watch out chatgpt there is a new player in town

--- agents/goob/goob.gd
+++ agents/goob/goob.gd
@@ -1,33 +1,35 @@
 extends Node2D
 
 @onready var health: Health = $Health
+
+var target: Node2D = null
 #@onready var player: Node2D = get_tree().get_first_node_in_group(&"player")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	health.damaged.connect(_on_health_damaged)
 	health.death.connect(_on_health_death)
-	$AnimatedSprite2D.play(&"default")
+	#$GoobSprite.play(&"default")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-	#if Input.is_action_just_pressed(&"action_debug"):
-		#fire_waves()
-
-func fire_waves(target: Node2D, speed: float = 0.3) -> void:
-	for i in range(3):
-		var wave = load(&"res://agents/gravity_wave/gravity_wave.tscn").instantiate()
-		wave.wave_size = 2 - i
-		wave.position = $MouthMarker.position + Vector2(-5, -5)
-		wave.rotation = (target.global_position - $MouthMarker.global_position).angle()
-		add_child(wave)
-		await get_tree().create_timer(speed).timeout
+func fire_wave(size: int, speed: float = 0.3) -> void:
+	if target == null:
+		return
+	var wave = load(&"res://agents/gravity_wave/gravity_wave.tscn").instantiate()
+	wave.wave_size = size
+	wave.position = $MouthMarker.position + Vector2(-5, -5)
+	wave.rotation = (target.global_position - $MouthMarker.global_position).angle()
+	add_child(wave)
+	await get_tree().create_timer(speed).timeout
 
 func _on_health_death() -> void:
 	$DeathSound.play()
-	$DeathSound.finished.connect(func(): queue_free())
+	await $DeathSound.finished
+	queue_free()
 
 func _on_health_damaged(target: Node, amount: float) -> void:
 	if target.is_ancestor_of(self) or target == self:
-		$AnimatedSprite2D/DamageIndicator.hit()
+		$GoobSprite/DamageIndicator.hit()
 		$HurtSound.play()
+
+func is_awake() -> bool:
+	return health.max_health > health.get_current()
