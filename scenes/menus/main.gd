016a91ed72eb10e6fe6d7214db87f545fc18b4f8
shodan <mega@ioexception.at>
2024-10-07 13:00:51 +0200

    title screen

--- scenes/menus/main.gd
+++ scenes/menus/main.gd
@@ -1,11 +1,4 @@
 extends Container
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_play_button_pressed() -> void:
+	SceneManager.change_scene("res://scenes/levels/0/cutscene.tscn")
