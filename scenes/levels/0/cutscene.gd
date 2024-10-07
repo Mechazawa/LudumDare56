413a7e2ed3d0f495dbf7b76654cef9c099621392
shodan <mega@ioexception.at>
2024-10-07 02:47:17 +0200

    wip

--- scenes/levels/0/cutscene.gd
+++ scenes/levels/0/cutscene.gd
@@ -2,5 +2,9 @@ extends Node
 
 @export var next_scene: PackedScene
 
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("action_tool"):
+		_on_animation_player_animation_finished("none")
+
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	SceneManager.change_scene(next_scene.resource_path)
