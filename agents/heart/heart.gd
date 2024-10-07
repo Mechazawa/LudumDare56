1343060b9158976f45f6836c8c4b89131cb17883
shodan <mega@ioexception.at>
2024-10-07 03:18:25 +0200

    pacemaker level stub

--- agents/heart/heart.gd
+++ agents/heart/heart.gd
@@ -1,16 +1,33 @@
+@tool
 extends StaticBody2D
 
 @onready var health: Health = get_tree().get_first_node_in_group("patient_health")
 @onready var random_effect: RandomEffect = $RandomEffect
 
-@export_category("Damage")
-var collide_damage: float = 1
+@export var has_pacemaker: bool = false:
+	set(value):
+		has_pacemaker = value
+		_setup_heart()
+@export var collide_damage: float = 1
 
 func _ready() -> void:
 	if health != null:
 		health.damaged.connect(_on_health_damaged)
 		health.death.connect(_on_health_death)
 	$AnimatedSprite2D.play(&"default")
+	_setup_heart()
+
+func _safe_set(target: Node2D, field: String, value: bool) -> void:
+	if target != null:
+		target[field] = value
+
+func _setup_heart() -> void:
+	_safe_set($PacemakerColisionPolygon1, 'disabled', not has_pacemaker)
+	_safe_set($PacemakerColisionPolygon2, 'disabled', not has_pacemaker)
+	_safe_set($PacemakerColisionPolygon3, 'disabled', not has_pacemaker)
+	_safe_set($CollisionPolygon2D, 'disabled', has_pacemaker)
+	_safe_set($PacemakerSprite, 'visible', has_pacemaker)
+	_safe_set($PacemakerTntSprite, 'visible', has_pacemaker)
 
 func _process(delta: float) -> void:
 	pass
